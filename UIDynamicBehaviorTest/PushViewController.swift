82e17ec99cb4c1d7ba91b3516078ea72611d1870
Alex Neminsky <uamoder@icloud.com>
2017-02-20 10:07:46 +0200

    push behavior 2

--- UIDynamicBehaviorTest/PushViewController.swift
+++ UIDynamicBehaviorTest/PushViewController.swift
@@ -40,6 +40,14 @@ class PushViewController: UIViewController {
         return push
     } ()
     
+    lazy var collision: UICollisionBehavior = {
+        let collision = UICollisionBehavior(items: [self.box])
+        collision.collisionMode = .everything
+        collision.translatesReferenceBoundsIntoBoundary = true
+        //collision.addBoundary(withIdentifier: "ups" as NSCopying, for: UIBezierPath(rect: CGRect(x: 160, y: 300, width: 80, height: 80)))
+        return collision
+    } ()
+    
     lazy var rope: CAShapeLayer = {
         let rope = CAShapeLayer()
         rope.fillColor = UIColor.clear.cgColor
@@ -65,39 +73,65 @@ class PushViewController: UIViewController {
     func addPan(pan: UIPanGestureRecognizer) {
         
         let point = pan.location(in: view)
+        let state = pan.state
         
+        //behavior1With(point: point, state: state)
         
-        switch pan.state {
+        behavior2With(point: point, state: state)
+        
+
+}
+
+    func behavior1With(point: CGPoint, state: UIGestureRecognizerState) {
+        switch state {
         case .began:
             animator.removeAllBehaviors()
             print ("")
         case .changed:
-            
             box.center = point
-            
             let path = UIBezierPath()
             path.move(to: view.center)
             path.addLine(to: box.center)
             rope.path = path.cgPath
-            
-            
-            
             print ("")
         case .ended:
-            //rope.removeFromSuperlayer()
-            
             var distance = sqrt(pow(view.center.x - point.x, 2) + pow(view.center.y - point.y, 2))
             distance = max(distance, 10)
-            
             let angle = atan2(view.center.y - point.y,  view.center.x - point.x)
-            
             push.angle = angle
             push.magnitude = distance / 40
             push.active = true
-            
             animator.addBehavior(attach)
             animator.addBehavior(push)
-            
+            print ("")
+        default:
+            print ("")
+        }
+    
+    }
+
+    
+    var prevPoint: CGPoint?
+    func behavior2With(point: CGPoint, state: UIGestureRecognizerState) {
+        switch state {
+        case .began:
+            animator.removeAllBehaviors()
+            prevPoint = point
+            print ("")
+        case .changed:
+            box.center = point
+            //prevPoint = point
+            print ("")
+        case .ended:
+            var distance = sqrt(pow(view.center.x - point.x, 2) + pow(view.center.y - point.y, 2))
+            distance = max(distance, 10)
+            //let angle = atan2(view.center.y - point.y,  view.center.x - point.x)
+            let angle = atan2(point.y - prevPoint!.y,  point.x - prevPoint!.x)
+            push.angle = angle
+            push.magnitude = distance / 40
+            push.active = true
+            animator.addBehavior(push)
+            animator.addBehavior(collision)
             print ("")
         default:
             print ("")
